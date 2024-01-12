b69803b8f817b7962a742b6aeba6430f0f6910e4
Mateus M <mateusmamani123890@gmail.com>
2024-01-12 16:19:11 -0300

    continuação de onde parei

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
 import './index.css'
 import Aside from './components/Aside'
-
-import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Teste/>
+    <>
+      <div className='desktop'></div>
+
+      <div className='mobile'>
+        <Header />
+        <main>
+          <h3>Categorias</h3>
+          <div className='categorias-mobile'>
+            <Aside
+              title="Espetos"
+              alt="icone de espeto"
+              href="./src/assets/espeto-icon.png"
+            />
+            <Aside
+              title="Sobremesas"
+              alt="icone de bolo"
+              href="./src/assets/bolo-icon.png"
+            />
+            <Aside
+              title="Bebidas"
+              alt="icone de refrigerante"
+              href="./src/assets/refrigerante-icon.png"
+            />
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
 
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,12 +1,14 @@
+
 export default function Aside(props){
     return(
         <div className="card-categoria-mobile">
             <img
-             src={props.img}
-             alt={props.alt}
+                className="img-card-mobile"
+                src={props.href}
+                alt={props.alt}
             />
             <p>{props.title}</p>
-            <button className="bnt-card">&#62;</button>
+            <button className="btn-card">&#62;</button>
         </div>
     )
 }
