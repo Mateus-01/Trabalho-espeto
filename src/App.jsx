e2036af898284ab721b21f852aa2b9ea3bf86d6e
Mateus-01 <mateusmamani123890@gmail.com>
2024-01-20 13:24:53 -0300

    teste vercel 8.0

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Section from './components/Section'
 import Produtos from './components/Produtos'
 import './telaProduto.css'
 import TelaMain from './components/TelaMain'
+import EspetoIcon from "./assets/espeto-icon.png"
 
 const espetosLista = [
   {
